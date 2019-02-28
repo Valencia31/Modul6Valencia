171c25c1fc481c4f6e63226df7906d1a93c8e5bf
Valencia31 <46375072+Valencia31@users.noreply.github.com>
2019-02-28 20:51:33 +0700

    Add files via upload

---
+++ Latihan1(5).php
@@ -0,0 +1,13 @@
+<?php
+//membuat fungsi
+function perkenalan() {
+    echo "Assalamualaikum,";
+    echo "Perkenalkan, nama saya Valencia<br/>";
+    echo "Senang berkenalan dengaanmu<br/>";
+}
+//memanggil fungsi yang sudah dibuat
+perkenalan();
+echo "<hr>";
+//memanggilnya lagi
+perkenalan();
+?>
---
+++ pratikum.php
@@ -0,0 +1,7 @@
+<?php
+function balok($tinggi,$panjang,$lebar){
+    return $hasil = $tinggi*$panjang*$lebar;
+}
+echo "Volume balok adalah : ";
+echo balok(12,2,9);
+?>
