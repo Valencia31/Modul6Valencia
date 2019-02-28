9af1852e02ee9cb424bfabef7e60e509982ab886
Valencia31 <46375072+Valencia31@users.noreply.github.com>
2019-02-28 20:56:37 +0700

    Delete latihan4(5).php

--- latihan4(5).php
+++
@@ -1,9 +0,0 @@
-<?php
-function head() {
-    echo'
-    <link rel="stylesheet" type="text/css" href="style/style.css">
-    <link rel="stylesheet" type="text/css" href="style/font-awesome.css">';
-}
-//pemanggilan fungsi
-echo luas_lingkaran($r);
-?>
